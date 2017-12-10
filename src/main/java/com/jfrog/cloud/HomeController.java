5329223d759a8d25801f4ca17f1d9a194f5201d4
justin <fuhui_jurassic@163.com>
2017-12-10 17:31:19 +0800

    update return message

--- src/main/java/com/jfrog/cloud/HomeController.java
+++ src/main/java/com/jfrog/cloud/HomeController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @RequestMapping("/")
     public String hello() {
-        return "Hello";
+        return "Hello JFrog!";
     }
 }
