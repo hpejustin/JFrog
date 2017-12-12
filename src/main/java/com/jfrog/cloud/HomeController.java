9fc082bd5a2bb45e0b2d251f7d9de45a665b2c2e
justin <fuhui_jurassic@163.com>
2017-12-12 18:02:39 +0800

    update

--- src/main/java/com/jfrog/cloud/HomeController.java
+++ src/main/java/com/jfrog/cloud/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 
     @RequestMapping("/")
     public String hello() {
-        return "JFrog Artifactory!";
+        return "Welcome to JFrog Artifactory!";
     }
 }
