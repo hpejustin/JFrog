8eeda62edaee6a9cb93dece9729dc2a864800de1
justin <fuhui_jurassic@163.com>
2017-12-15 15:27:38 +0800

    welcome

--- src/main/java/com/jfrog/cloud/HomeController.java
+++ src/main/java/com/jfrog/cloud/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Welcome to JFrog Artifactory!";
+        return "Welcome to JFrog Artifactory!, Welcome to JFrog Artifactory!";
     }
 }
